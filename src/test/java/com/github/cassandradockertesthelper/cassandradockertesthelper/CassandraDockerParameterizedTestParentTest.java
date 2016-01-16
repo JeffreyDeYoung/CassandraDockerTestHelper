9b53f2b04199debe8891859381fe84217b7269a3
Jeffrey DeYoung <Jeffrey.J.DeYoung@gmail.com>
2016-01-15 19:40:26 -0700

    mostly javadoc, some asserts

--- src/test/java/com/github/cassandradockertesthelper/cassandradockertesthelper/CassandraDockerParameterizedTestParentTest.java
+++ src/test/java/com/github/cassandradockertesthelper/cassandradockertesthelper/CassandraDockerParameterizedTestParentTest.java
@@ -1,9 +1,23 @@
+/*
+ * Copyright 2016 Jeffrey DeYoung.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.cassandradockertesthelper.cassandradockertesthelper;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -14,11 +28,13 @@ import org.slf4j.LoggerFactory;
 /**
  * Test class for CassandraDockerParameterizedTestParent. Also is an example on
  * how to properly use CassandraDockerParameterizedTestParent and demonstrates
- * the features of this test library.
+ * the features of this test library. Look for comments that are marked with
+ * "*you must do this*" for hints on how to use
+ * CassandraDockerParameterizedTestParent correctly.
  *
  * @author jeffrey
  */
-public class CassandraDockerParameterizedTestParentTest extends CassandraDockerParameterizedTestParent
+public class CassandraDockerParameterizedTestParentTest extends CassandraDockerParameterizedTestParent//note that we are extending the parent here: *you must do this*
 {
 
     /**
@@ -26,21 +42,35 @@ public class CassandraDockerParameterizedTestParentTest extends CassandraDockerP
      */
     private static final Logger logger = LoggerFactory.getLogger(CassandraDockerParameterizedTestParentTest.class);
 
+    /**
+     * Constructor. Must follow this format. The file parameter is provided by
+     * the parameterized tests (defined by generateParameters in the parent
+     * class). *you must do this*
+     *
+     * @param dockerFile Parameter for this test. It's the docker file that
+     * represents the version of Cassandra we are testing.
+     */
     public CassandraDockerParameterizedTestParentTest(File dockerFile)
     {
-        super(dockerFile);
+        super(dockerFile);//call to super class to actually setup this test. *you must do this*
     }
 
+    /**
+     * Set up class method. This method is static and will run prior to any
+     * tests, but only once: at class establishment.
+     */
     @BeforeClass
     public static void setUpClass()
     {
+        /* if you want to set specific versions of cassandra you want your tests 
+         to run against, you must set the static variable "cassandraVersions" in 
+         a @BeforeClass method. *you must do this* if you want to set the 
+         cassandra versions. If you do not set this, the test will run against all
+         cassandra versions availible. 
+         */
         cassandraVersions = getCassandraVersions();//this breaks so many programming conventions that it makes my head hurt.
     }
 
-    @AfterClass
-    public static void tearDownClass()
-    {
-    }
 
     @Before
     public void setUp()
@@ -49,6 +79,12 @@ public class CassandraDockerParameterizedTestParentTest extends CassandraDockerP
         //String dockerId = super.spinUpNewCassandraDockerBox();
     }
 
+    /**
+     * Gets a simple String List of all the Cassandra versions we want to test
+     * against. See setUpClass() and related comments.
+     *
+     * @return A list of Cassandra versions to test against.
+     */
     public static List<String> getCassandraVersions()
     {
         List<String> versions = new ArrayList<>();
@@ -60,12 +96,24 @@ public class CassandraDockerParameterizedTestParentTest extends CassandraDockerP
         return versions;
     }
 
+    /**
+     * Example test with lots of method calls. 
+     */
     @Test
     public void testCassandraParamaterizedTests()
     {
-        logger.info("Testing with cassandra. Test name (with paramter name)" + super.getTestName());
-        logger.info("Testing cassandra version: " + super.getCassandraVersion());
-        assertTrue(true);
+        assertNotNull(super.getTestName());
+        logger.info("Testing with Cassandra. Test name (with paramter name)" + super.getTestName());//Shows how to get the test name with the current parameter
+        assertNotNull(super.getCassandraVersion());
+        logger.info("Testing Cassandra version: " + super.getCassandraVersion());//Shows how to get the Cassandra version we are testing against.
+        String firstDockerId = super.spinUpNewCassandraDockerBox();//creates a NEW docker instance with the specified version of cassandra for this parameterized test run.
+        assertNotNull(firstDockerId);
+        logger.info("Created new Cassandra Docker box: " + firstDockerId);
+        //note, we are NOT spining down the box we created. We are relying on the parent class to do that automatically at the end of this test
+        String firstIp = DockerHelper.getDockerIp(firstDockerId);//we can get the ip from the docker helper based on the instance id
+        assertNotNull(firstIp);
+        
+        
     }
 
 //    /**
